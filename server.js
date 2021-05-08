7dacd3164cce42bc4b8f8423e52042b57d2646dc
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-08 14:11:07 -0400

    added express, the port and definded the routes for the project

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, () => console.log(`Server started running on port ${PORT}`));
