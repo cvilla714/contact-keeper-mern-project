dc2c207218cb7b055bcc051dabd9c936e7da18ab
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-10 18:15:58 -0400

    switched to import  instead of require

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
+import connectDB from './config/db.js';
+import usersRoutes from './routes/users.js';
+import authRoutes from './routes/auth.js';
+import contactsRoutes from './routes/contacts.js';
 
 // Connect Database
 connectDB();
@@ -10,8 +15,11 @@ connectDB();
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
 
 //Define routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+// app.use('/api/users', require('./routes/users'));
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', usersRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/contacts', contactsRoutes);
 
 app.listen(PORT, () => console.log(`Server started running on port ${PORT}`));
