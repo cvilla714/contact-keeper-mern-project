6fda535e56acfaf5e33884dd7d660033372691ae
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-11 11:55:58 -0400

    applied async and await,added try and catch to validate if the email is valid and password and if they match with the information from the database

--- routes/auth.js
+++ routes/auth.js
@@ -19,8 +19,47 @@ router.get(
     body('email', 'Please include a valid email').isEmail(),
     body('password', 'Password is required').exists(),
   ],
-  (req, res) => {
-    res.send('Get logged in user');
+  async (req, res) => {
+    // res.send('Get logged in user');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid email address' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid password' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        jwtSecret,
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
