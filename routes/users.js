3f879b77b3a2ce113e4ab4aa5079300528cb6a76
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-11 01:49:32 -0400

    imported bcrypt and created async and await to create a new user, and encrypt password to mongoDB

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 import User from '../models/User.js';
 import { body, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
 
 //@route    POST api/users
 //@desc     Register a user
@@ -14,14 +15,40 @@ router.post(
     body('email', 'Please include a valid email').isEmail(),
     body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // res.send('Register a user');
     // res.send(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+    // res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
