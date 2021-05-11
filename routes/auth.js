04781ba9c9be39a6cab2bc6a8d8f6a163c8b5abb
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-11 11:46:37 -0400

    imported the user model, express validator , bcrypt ,jwt and dotevn , also added validation for email and password

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,28 @@
 import express from 'express';
 // const express = require('express');
 const router = express.Router();
+import User from '../models/User.js';
+import { body, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+const lb = dotenv.config();
+const jwtSecret = process.env.JWT_SECRET;
 
 //@route    GET api/auth
 //@desc     Get logged in user
 //@access   Private  because you need authentication
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
-});
+router.get(
+  '/',
+  [
+    body('email', 'Please include a valid email').isEmail(),
+    body('password', 'Password is required').exists(),
+  ],
+  (req, res) => {
+    res.send('Get logged in user');
+  }
+);
 
 //@route    POST api/auth
 //@desc     Auth user & get token
