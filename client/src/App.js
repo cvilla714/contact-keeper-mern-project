8cc22a68df16337bb9016eace5c33eba21dc659e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-12 18:42:54 -0400

    imported Reqct, Fragmet,also BrowserRouter, Route and Switch to create the routes and imported the Home and About component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 
 const App = () => {
-  return <div className="App">My App</div>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact about="/about" component={About} />
+          </Switch>
+        </div>
+        My App
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
