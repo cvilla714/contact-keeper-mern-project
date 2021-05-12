89bdb021dffa138077ef08d28aac2991cae73943
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-12 00:05:26 -0400

    imported express validator, checkToken and added it as a second parameter applied async and await and added try and catch handle errors

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,24 @@
 import express from 'express';
 // const express = require('express');
 const router = express.Router();
+import checkToken from '../middleware/auth.js';
+import User from '../models/User.js';
+import Contact from '../models/Contact.js';
+import { body, validationResult } from 'express-validator';
 
 //@route    GET api/contacts
 //@desc     GET all users contacts
 //@access   Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', checkToken, async (req, res) => {
+  // res.send('Get all contacts');
+
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route    POST api/contacts
