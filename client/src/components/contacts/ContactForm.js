23ca43eea3e1ddb1eee3fe06eda9b6aa4e48c57d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-13 23:35:32 -0400

    created the contat form, initalize the state with a single object , destructure it and grabed  the value of the input field from each input

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal',
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = (e) => setContact({ ...contact, [e.target.name]: [e.target.value] });
+  return (
+    <form>
+      <h2 className="text-primary">Add Contact</h2>
+      <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} />
+      <input type="email" placeholder="Email" name="email" value={email} onChange={onChange} />
+      <input type="text" placeholder="Phone" name="phone" value={phone} onChange={onChange} />
+      <h5>Contact Type</h5>
+      <input type="radio" name="type" value="personal" checked={type === 'personal'} />
+      Personal
+      <input type="radio" name="type" value="professional" checked={type === 'professional'} />
+      Professional
+      <div>
+        <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
