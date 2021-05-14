a472f24ed4ef536514fa5507a2bb5843a8e86ff4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-14 02:11:11 -0400

    created the add_contact case

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    default:
+      return state;
+  }
+};
