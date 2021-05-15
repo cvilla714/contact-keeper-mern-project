dc5eee94dee9fbe90920787b235b7f9a6bd6ea14
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-15 02:54:54 -0400

    added the updateContact action that will dispatch the action update contact and pass the contact to the payload,also added it to the provider to access it from anywhere

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -69,6 +69,9 @@ const ContactState = (props) => {
     dispatch({ type: CLEAR_CURRENT });
   };
   // Update Contact
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   //Filter Contacts
 
@@ -83,6 +86,7 @@ const ContactState = (props) => {
         deleteContact,
         setCurrent,
         clearCurrent,
+        updateContact,
       }}
     >
       {props.children}
