7af831b9ecf6fc1396915d64f629cbb0e0a99ab0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-16 02:31:18 -0400

    created the login form

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmi = (e) => {
+    e.preventDefault();
+    console.log('Logged in ');
+  };
+
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Login</span>
+      </h1>
+      <form onSubmi={onSubmi}>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" value={password} onChange={onChange} />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary btn-block" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
