a810a47460dd074b07db60059f0acaec81bc7259
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-16 02:22:37 -0400

    imported AuthState and wrapped the app with the component to have access to the state and actions, also imported the Register component and created the route

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,30 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact about="/about" component={About} />
-            </Switch>
-          </div>
-          My App
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+              </Switch>
+            </div>
+            My App
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
