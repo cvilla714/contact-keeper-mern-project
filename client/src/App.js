d83f7b41e5bde97c2d6a7a3d1845871d7c62d4c6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-08 23:43:09 -0400

    imported Alerts and wrapped it before the switch

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
@@ -19,6 +21,7 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className="container">
+                <Alerts />
                 <Switch>
                   <Route exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
