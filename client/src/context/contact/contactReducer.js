601b86154b0845d58fbb2250bda8d7885c8abd09
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 15:04:32 -0400

    created the getContact actions and also added loading false to add contact, update contact and deleted contacts actions

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,22 +1,31 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER, CONTACT_ERROR } from '../types';
+import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER, CONTACT_ERROR, GET_CONTACT } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACT:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
+        loading: false,
       };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map((contact) => (contact.id === action.payload.id ? action.payload : contact)),
+        loading: false,
       };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter((contact) => contact.id !== action.payload),
+        loading: false,
       };
     case SET_CURRENT:
       return {
