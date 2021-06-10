f02a76f05835c49172be2d5b457a30b22a33913a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 01:43:31 -0400

    added logout functionality created authorized links and guestlinks

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => {
+    logout();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt">
+            <span className="hide-sm">Logout</span>
+          </i>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
   return (
     <div className="navbar bg-primary">
       <h1>
         <i className={icon}>{title}</i>
       </h1>
       <ul>
-        <li>
+        {isAuthenticated ? authLinks : guestLinks}
+        {/* <li>
           <Link to="/">Home</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <Link to="/register">Register</Link>
         </li>
         <li>
           <Link to="/login">Login</Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
