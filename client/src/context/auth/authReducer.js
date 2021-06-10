d0b2746844d5ee0139e51bef13867f6eeebff01f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 01:18:23 -0400

    added the login success and login fail scenarios

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -21,6 +21,7 @@ export default (state, action) => {
       };
 
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -30,6 +31,7 @@ export default (state, action) => {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
