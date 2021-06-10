6eb2ebb615351029cf7c2c1d853473542e8189d5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 16:58:58 -0400

    added ty and catch to deleteContacts and use async await to send to MongoDB the correct id for the uset that will be deleted

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -60,8 +60,16 @@ const ContactState = (props) => {
   };
 
   // Delete Contact
-  const deleteContact = (id) => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async (id) => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg,
+      });
+    }
   };
 
   // Clear Contacts
