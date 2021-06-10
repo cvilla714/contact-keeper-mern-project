7728f15a59f2bf7d1f5721a5f56e89907ad2f445
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 12:38:34 -0400

    running the project on second computer, included new environmental variables

--- routes/users.js
+++ routes/users.js
@@ -14,66 +14,58 @@ const jwtSecret = process.env.JWT_SECRET;
 //@route    POST api/users
 //@desc     Register a user
 //@access   Public because you need authentication
-router.post(
-  '/',
-  [
-    body('name', 'Please add name').not().isEmpty(),
-    body('email', 'Please include a valid email').isEmail(),
-    body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
-  ],
-  async (req, res) => {
-    // res.send('Register a user');
-    // res.send(req.body);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    // res.send('passed');
-    const { name, email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return res.status(400).json({ msg: 'User already exists' });
-      }
-
-      user = new User({
-        name,
-        email,
-        password,
-      });
-
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      // res.send('User saved');
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-
-      jwt.sign(
-        payload,
-        jwtSecret,
-        {
-          expiresIn: 360000,
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send('Server Error');
-    }
+router.post('/', [body('name', 'Please add name').not().isEmpty(), body('email', 'Please include a valid email').isEmail(), body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })], async (req, res) => {
+  // res.send('Register a user');
+  // res.send(req.body);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+  // res.send('passed');
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    // res.send('User saved');
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      jwtSecret,
+      {
+        expiresIn: 360000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 export default router;
 // module.exports = router;
