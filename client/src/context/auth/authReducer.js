2b667763f5a9bf162b3705ce6abe6a8c62a59ed2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 00:41:27 -0400

    added the action for user loaded and set the isAuhenticated to true, the loading to false and user to the action paylaod that is the information coming from the api

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,6 +12,14 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
+
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
@@ -21,6 +29,7 @@ export default (state, action) => {
         loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
