4b8ea042d6b3952de2cf649fc5fb0a40fb258428
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 17:26:37 -0400

    added the async and await to update the contact sending the put request to the database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -72,6 +72,29 @@ const ContactState = (props) => {
     }
   };
 
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config,
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg,
+      });
+    }
+  };
+
   // Clear Contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -84,10 +107,6 @@ const ContactState = (props) => {
   // Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter Contacts
