4371f7973e92ca3af829b020f13b8d1134caba58
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 01:17:58 -0400

    created the login method, passed props to use useEffect to redirect to home route, and passed the invalid credentials if an error should come up

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () => {
+const Login = (props) => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
+    if (error === 'Invalid Credentials') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: '',
     password: '',
@@ -10,9 +30,17 @@ const Login = () => {
 
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-  const onSubmi = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    console.log('Logged in ');
+    // console.log('Logged in ');
+    if (email === '' || password === '') {
+      setAlert('Please fill in all fields', 'dange');
+    } else {
+      login({
+        email,
+        password,
+      });
+    }
   };
 
   return (
@@ -20,7 +48,7 @@ const Login = () => {
       <h1>
         Account <span className="text-primary">Login</span>
       </h1>
-      <form onSubmi={onSubmi}>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
           <input type="email" name="email" value={email} onChange={onChange} />
