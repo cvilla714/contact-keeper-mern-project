bf6841a84c05c10460caa70bc43f21b04091b09f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-10 00:42:14 -0400

    added the method loadUser and set the token to the local storage if not present

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -25,6 +26,22 @@ const AuthState = (props) => {
 
   // Load User
 
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (error) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
   // Register User
   const register = async (formdData) => {
     const config = {
@@ -40,6 +57,8 @@ const AuthState = (props) => {
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+
+      loadUser();
     } catch (error) {
       dispatch({
         type: REGISTER_FAIL,
@@ -65,6 +84,7 @@ const AuthState = (props) => {
         error: state.error,
         register,
         clearErrors,
+        loadUser,
       }}
     >
       {props.children}
