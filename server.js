08617d97749081dff3c9df21deddd1b024cbea74
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-11 20:54:33 -0400

    delete unnecessary commented code and added variabe to hold the value of __dirname

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const connectDB = require('./config/db');
 import connectDB from './config/db.js';
 import path from 'path';
-// const path = require('path');
+const __dirname = path.resolve();
 import usersRoutes from './routes/users.js';
 import authRoutes from './routes/auth.js';
 import contactsRoutes from './routes/contacts.js';
@@ -16,13 +15,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) => res.send('Hello World'));
-// app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
-
 //Define routes
-// app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', usersRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/contacts', contactsRoutes);
@@ -32,8 +25,10 @@ if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+  app.get(
+    '*',
+    (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.hmtml')),
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
   );
 }
 
